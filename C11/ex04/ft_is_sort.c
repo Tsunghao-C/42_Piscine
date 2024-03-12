fb064fb8780651fd1e8d38e971617910dabe7e2c
Tsunghao <tsunghao.chen12@gmail.com>
2024-03-12 15:56:38 +0100

    ex04 submit

---
+++ C11/ex04/ft_is_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int	ft_is_sort(int *tab, int length, int(*f)(int, int))
+{
+	int	i;
+
+	i = 0;
+	while (i < (length - 1))
+	{
+		if (f(tab[i], tab[i + 1]) > 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/*
+int	int_cmp(int a, int b)
+{
+	return (a - b);
+}
+
+int	main(void)
+{
+	int	tab[10] = {1, 2, 3, 7, 5, 6, 7, 8, 9, 9};
+	int	length = 10;
+	int	output;
+
+	output = ft_is_sort(tab, length, &int_cmp);
+	printf("Result is: %d\n", output);
+	return (0);
+}*/
