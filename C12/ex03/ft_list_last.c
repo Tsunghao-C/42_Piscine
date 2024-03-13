c8254e1e692d936a9df37bab9aa39c58b220bc53
Tsunghao <tsunghao.chen12@gmail.com>
2024-03-13 14:47:58 +0100

    ex03 submit

---
+++ C12/ex03/ft_list_last.c
@@ -0,0 +1,38 @@
+#include "ft_list.h"
+#include <stdio.h>
+
+t_list	*ft_list_last(t_list *begin_list)
+{
+	t_list	*last;
+
+	last = begin_list;
+	while (last != NULL)
+	{
+		if ((*last).next == NULL)
+			return (last);
+		last = (*last).next;
+	}
+	return (last);
+}
+/*
+t_list  *ft_create_elem(void *data);
+void    ft_list_push_front(t_list **begin_list, void *data);
+
+int	main(void)
+{
+	t_list	*lst;
+	t_list	*last;
+	int	a = 3;
+	int	b = 4;
+	int	i = 0;
+
+	lst = ft_create_elem(&a);
+	while (i < 10)
+	{
+		ft_list_push_front(&lst, &b);
+		i++;
+	}
+	last = ft_list_last(lst);
+	printf("value of last list is: %d\n", *(int *)(*last).data);
+	return (0);
+}*/
